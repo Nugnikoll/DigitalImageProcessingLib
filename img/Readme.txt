70d1d0cd9293370a10b6f50bab977ee58d5eb0df
Rick <1300012743@pku.edu.cn>
2018-10-18 02:25:56 +0800

    Add some sample images.

---
+++ img/Readme.txt
@@ -0,0 +1,4 @@
+JPEG_example文件都来自https://en.wikipedia.org/wiki/JPEG
+apple.png和fruit.png来自http://www.datagenetics.com/blog/december32013/index.html
+simple.jpg来自https://www.impulseadventure.com/photo/jpeg-huffman-coding.html
+scene.jpg来自摄影师北纬30°
--- sample/equalize.py
+++ sample/equalize.py
@@ -4,10 +4,8 @@ from PIL import Image as image;
 import numpy as np;
 from jpeg import jpeg;
 
-#img = image.open("../img/IMG_20181016_020153.jpg");
-img = image.open("../img/IMG_20181016_023212.jpg");
-#img = image.open("../img/IMG_20181016_020223.jpg");
-#img = image.open("../img/738b4710b912c8fcc7977bd3f1039245d6882143.jpg");
+#img = image.open("../img/dark.jpg");
+img = image.open("../img/scene.jpg");
 data = np.array(img.getdata(), dtype = np.uint8);
 data = data.reshape((img.height, img.width, 3));
 
@@ -16,9 +14,9 @@ plt.show();
 plt.close();
 
 result = data.copy();
-result[:, :, 0] = jpeg.equalize(result[:, :, 0].astype(np.int32), np.min(result[:, :, 0]), np.max(result[:, :, 0]));
-result[:, :, 1] = jpeg.equalize(result[:, :, 1].astype(np.int32), np.min(result[:, :, 1]), np.max(result[:, :, 1]));
-result[:, :, 2] = jpeg.equalize(result[:, :, 2].astype(np.int32), np.min(result[:, :, 2]), np.max(result[:, :, 2]));
+result[:, :, 0] = jpeg.equalize(result[:, :, 0].astype(np.int32), int(np.min(result[:, :, 0])), int(np.max(result[:, :, 0])));
+result[:, :, 1] = jpeg.equalize(result[:, :, 1].astype(np.int32), int(np.min(result[:, :, 1])), int(np.max(result[:, :, 1])));
+result[:, :, 2] = jpeg.equalize(result[:, :, 2].astype(np.int32), int(np.min(result[:, :, 2])), int(np.max(result[:, :, 2])));
 result = result.astype(np.uint8);
 
 plt.imshow(result);
@@ -27,8 +25,8 @@ plt.close();
 
 result = jpeg.rgb2ycc(data.astype(np.int32));
 result[:, :, 0] = jpeg.equalize(result[:, :, 0].astype(np.int32), -128, 128);
-result[:, :, 1] = jpeg.equalize(result[:, :, 1].astype(np.int32), np.min(result[:, :, 1]), np.max(result[:, :, 1]));
-result[:, :, 2] = jpeg.equalize(result[:, :, 2].astype(np.int32), np.min(result[:, :, 2]), np.max(result[:, :, 2]));
+result[:, :, 1] = jpeg.equalize(result[:, :, 1].astype(np.int32), int(np.min(result[:, :, 1])), int(np.max(result[:, :, 1])));
+result[:, :, 2] = jpeg.equalize(result[:, :, 2].astype(np.int32), int(np.min(result[:, :, 2])), int(np.max(result[:, :, 2])));
 result = jpeg.ycc2rgb(result);
 result[result > 255] = 255;
 result[result < 0] = 0;
